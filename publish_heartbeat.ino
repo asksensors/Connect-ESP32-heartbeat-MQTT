5e9099824536803aaa8e7b1c2dc445ba66c591e0
AskSensors <42293557+asksensors@users.noreply.github.com>
2020-04-06 22:06:07 +0100

    Update publish_heartbeat.ino

--- publish_heartbeat.ino
+++ publish_heartbeat.ino
@@ -21,8 +21,8 @@ unsigned int mqtt_port = 1883;
 // KY039 defines
 #define TAB_LENGTH        4
 #define RISE_THRESHOLD  5
-#define CALIB_OFFSET  0 // change this offset after calibration
-
+#define CALIB_OFFSET  0
+  
 WiFiClient askClient;
 PubSubClient client(askClient);
 
@@ -50,56 +50,56 @@ void setup() {
 }
 
 void loop() {
-
+  
+  // variables declaration
   float analog_Tab[TAB_LENGTH], analog_sum;
-  float last, analog_average, start;
-  float KY039_data, first, second, third, before;
+  float KY039_data, last_heartbeat, before_heartbeat, analog_average;
+  float beat_first, beat_second, beat_third ;
   bool rising;
   int rise_count, n_reads;
-  long int last_beat, now, ptr;
+  long int last_beat, timer_now, timer_start, ptr;
+
   // WiFi connection 
   if (!client.connected()) 
     reconnect();
   client.loop();
   // Init variables
   for (int i = 0; i < TAB_LENGTH; i++)  analog_Tab[i] = 0;
-  analog_sum = 0;
-  ptr = 0;
+  analog_sum = 0, ptr = 0;
+   
    while(1) {
      // calculate an average of the sensor during a 20 ms period to eliminate the 50 Hz noise caused by electric light
      n_reads = 0;
-     start = millis();
      analog_average = 0.;
+     timer_start = millis();
      do {
        analog_average += analogRead(A0);
        n_reads++;
-       now = millis();
-     }
-     while (now < start + 20);  
-     analog_average /= n_reads;  // we got an average
-     // Add the newest measurement to an array and subtract the oldest measurement from the array
-     // to maintain a sum of last measurements
+       timer_now = millis();
+     } while (timer_now < timer_start + 20);  
+     analog_average /= n_reads;  //  average
+     
+     // Add the newest measurement to FIFO array (subtract the oldest measurement)
      analog_sum -= analog_Tab[ptr];
      analog_sum += analog_average;
      analog_Tab[ptr] = analog_average;
-     last = analog_sum / TAB_LENGTH;
-     // now last holds the average of the values in the array
+     last_heartbeat = analog_sum / TAB_LENGTH;
      // check for a rising curve (= a heart beat)
-     if (last > before) {
+     if (last_heartbeat > before_heartbeat) {
        rise_count++;
        if (!rising && rise_count > RISE_THRESHOLD) {
         // we have detected a rising curve, which implies a heartbeat.
-        // Record the time since last beat, keep track of the two previous times (first, second, third) to get a weighed average.
-        // The rising flag prevents us from detecting the same rise more than once.
-        rising = true;
-        first = millis() - last_beat;
+        rising = true;         // The rising flag prevents us from detecting the same rise more than once.
+        beat_first = millis() - last_beat;
         last_beat = millis();
         // Calculate the weighed average of heartbeat rate according to the three last beats
-        KY039_data = 60000. / (0.4 * first + 0.3 * second + 0.3 * third)+ CALIB_OFFSET;
+        // Filter equation reference: https://create.arduino.cc/projecthub/Johan_Ha/from-ky-039-to-heart-rate-0abfca
+        KY039_data = 60000. / (0.4 * beat_first + 0.3 * beat_second + 0.3 * beat_third)+ CALIB_OFFSET;
         Serial.print(KY039_data);
         Serial.println(" BPM\n"); // Unit
-        third = second;
-        second = first;
+        // Record the time since last beat, keep track of the two previous times (beat_first, beat_second, beat_third) to get a weighed average.
+        beat_third = beat_second;
+        beat_second = beat_first;
         Serial.println("********** Publish MQTT data to ASKSENSORS");
         char mqtt_payload[30] = "";
         snprintf (mqtt_payload, 30, "m1=%f", KY039_data);
@@ -118,7 +118,7 @@ void loop() {
        rising = false;
        rise_count = 0;
      }
-     before = last;
+     before_heartbeat = last_heartbeat;
      ptr++;
      ptr %= TAB_LENGTH;
    }
